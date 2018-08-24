b9ba1da036b08f7e3fd8d1ddba7bfee6641cf441
matrinot <24818322+matrinot@users.noreply.github.com>
2018-08-24 20:57:38 +0200

    test address update

---
+++ packages/core-test-utils/__tests__/generators/transactions.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const generateTransactions = require('../../lib/generators/transactions')
+const { TRANSACTION_TYPES } = require('../../../crypto/lib/constants')
+
+describe('generateTransactions', () => {
+  it('should be a function', () => {
+    expect(generateTransactions).toBeFunction()
+  })
+
+  it('should create transfer transactions for devnet', () => {
+    const devnetAddress = 'PL2dXvNtTg2bHY88e4ihnNxjNoJiu3xbK4'
+    const transactions = generateTransactions(
+      'devnet',
+      TRANSACTION_TYPES.TRANSFER,
+      undefined,
+      devnetAddress
+    )
+
+    for (let i = 0; i < transactions.length; i++) {
+      expect(transactions[i]).toMatchObject({ recipientId: devnetAddress })
+    }
+  })
+})
